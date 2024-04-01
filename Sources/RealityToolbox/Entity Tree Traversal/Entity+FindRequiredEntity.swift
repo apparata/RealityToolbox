3206a3bddaea822316670a8918f1a906a3633de6
Martin Johannesson <mjohannesson@gmail.com>
2024-04-01 15:39:24 +0200

    Added find required entity.

---
+++ Sources/RealityToolbox/Entity Tree Traversal/Entity+FindRequiredEntity.swift
@@ -0,0 +1,24 @@
+import Foundation
+import RealityKit
+
+extension Entity {
+
+    /// Returns the named entity if it's in the tree, or crashes with a fatal error otherwise.
+    public func findRequiredEntity(named name: String) -> Entity {
+        guard let entity = findEntity(named: name) else {
+            fatalError("The required entity \(name) doesn't cannot be found")
+        }
+        return entity
+    }
+}
+
+extension Optional<Entity> {
+
+    /// Unwraps the entity on purpose or crashes. Meant for entities that must exist.
+    public var required: Entity {
+        guard let entity = self else {
+            fatalError("The required entity is nil")
+        }
+        return entity
+    }
+}
