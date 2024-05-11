6c0ef248eda5958b857e495702b76eafd1c7403e
Martin Johannesson <mjohannesson@gmail.com>
2024-05-12 01:56:46 +0200

    Improve AnimationPlaybackController.completion() method so that it specifies the event source as the entity.

--- Sources/RealityToolbox/Animation/AnimationPlaybackController+Completion.swift
+++ Sources/RealityToolbox/Animation/AnimationPlaybackController+Completion.swift
@@ -5,10 +5,13 @@ extension AnimationPlaybackController {
 
     /// Listens for the `PlaybackCompleted` event and then returns.
     @MainActor public func completion() async {
-        guard let scene = entity?.scene else {
+        guard let entity else {
             return
         }
-        let publisher = scene.publisher(for: AnimationEvents.PlaybackCompleted.self)
+        guard let scene = entity.scene else {
+            return
+        }
+        let publisher = scene.publisher(for: AnimationEvents.PlaybackCompleted.self, on: entity)
         _ = await publisher.values.first(where: { [weak self] event in
             event.playbackController == self
         })
